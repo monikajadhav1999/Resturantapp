71969ee34e87a8a93eb2fcdc7915a2cc93245723
monika jadhav <monika.jadhav631@gmail.com>
2023-07-14 12:24:55 +0530

    first res commit

---
+++ src/component/menuApi.js
@@ -0,0 +1,297 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "30₹",
+      description:
+        "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+    },
+
+    {
+      id: 2,
+      image: "images/dhokla.jpg",
+      name: "Dhokla",
+      category: "breakfast",
+      price: "50₹",
+      description:
+      "Dhokla is a savoury sponge dish that is native to the Indian state of Gujarat and parts of adjacent states, and is popular throughout the country. It is made with a fermented batter that is steamed to a cake-like consistency."
+    },
+     
+    {
+      id: 3,
+      image: "images/medu vada.jpg",
+      name: "Medu Vada",
+      category: "breakfast",
+      price: "20₹",
+      description:
+      "Vada is a category of savoury fried snacks native to South India. Vadas can be described variously as fritters, cutlets, or dumplings. Alternative names for this food include vadai, vade, and bada."
+    },
+
+    {
+      id: 4,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "breakfast",
+      price: "20₹",
+      description:
+       "Samosas are a staple appetizer, snack, and street food in Indian cuisine. Indian samosas can be filled with various ingredients, from spiced peas and potatoes to chicken to chickpeas. No matter what's inside this crispy and deep-fried dough, no samosa is complete without its complementary sauce." 
+         },
+     {
+          id: 5,
+          image: "images/kachori.jpg",
+          name: "Kachori",
+          category: "breakfast",
+          price: "20₹",
+          description:
+          "Kachori is Indian snack shaped like a round flattened ball, consisting of a flour-based shell with different fillings. The most popular filling consists of yellow moong dal, besan flour, pepper, chili powder, cumin seeds, and other spices."
+        },
+    {
+      id: 6,
+      image: "images/dosa.jpg",
+      name: "Dosa",
+      category: "breakfast",
+      price: "40₹",
+      description:
+      "A dosa is South Indian, fermented crepe made from rice batter and black lentils. Masala Dosa, specifically, is when you stuff it with a lightly cooked filling of potatoes, fried onions and spices."
+        },
+
+    {
+      id: 7,
+      image: "images/Pav bhaji.jpg",
+      name: "Pav bhaji",
+      category: "breakfast",
+      price: "50₹",
+      description:
+      "Pav bhaji is a spiced mixture of mashed vegetables in a thick gravy served with bread. Vegetables in the curry may commonly include potatoes, onions, carrots, chillies, peas, bell peppers and tomatoes. Street sellers usually cook the curry on a flat griddle (tava) and serve the dish hot."
+               },
+    {
+       id: 8,
+      image: "images/paratha.jpg",
+       name: "Paratha",
+      category: "breakfast",
+      price: "30₹",
+      description:
+      "Paratha is a good source of dietary fiber, and an excellent source of vitamin D, selenium, and manganese."
+        },                
+  
+    {
+      id: 9,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "40₹",
+      description:
+           "When fresh corn is at its peak in the summer months, we’re inclined to simply cook it and serve on the cob. Nothing wrong with that, but a world of inspiration awaits you when you want something different."    },
+      {
+         id: 1,
+         image: "../images/rajmarice.jpg",
+         name: "Rajma Rice",
+         category: "lunch",
+         price: "120₹",
+         description:
+                    "“Rajma Chawal has been a part of Indian cuisine for centuries. In modern times, there has been a growing appreciation for protein-rich meals. When it comes to Rajma and Rice, each of them have different amino acids, which when comes together form a complete package of amino acids."
+       },
+       {
+        id: 2,
+        image: "../images/chola.jpg",
+        name: "chola",
+        category: "lunch",
+        price: "80₹",
+        description:
+           "Fiber is important for digestive health, blood sugar control, and reducing the risk of chronic diseases such as heart disease and diabetes. Carbohydrates: Chickpeas are a good source of complex carbohydrates, which provide sustained energy and help to regulate blood sugar levels."    },
+      
+      
+       {
+        id: 3,
+        image: "../images/daal bati.jpg",
+        name: "Dal Baati Churma",
+        category: "lunch",
+        price: "80₹",
+        description:
+                   "Dal Bati Churma is the most popular dish in Rajasthani cuisine. It is made of three components of bati, dal, and churma. Dal is lentils, bati is a baked wheat ball, and churma is powdered sweetened cereal."
+      },
+      {
+        id: 4,
+        image: "../images/Dal Tadka.jpg",
+        name: "Dal Tadka",
+        category: "lunch",
+        price: "100₹",
+        description:
+                   "Your dal or sambar tempering is incomplete without finely chopped coriander leaves. And if you think it is just to enhance the taste, let us tell you that it also helps improve the digestive system. According to health experts, it has antispasmodic properties that further relax the contracted digestive muscles."
+      },
+      {
+        id: 5,
+        image: "../images/paneer.jpg",
+        name: "Panner Tomato",
+        category: "lunch",
+        price: "150₹",
+        description:
+                   "Paneer is an Indian cheese that's made from curdled milk and some sort of fruit or vegetable acid like lemon juice. There are some distinct traits that make Paneer the cheese that it is: It's an unaged cheese."
+      },
+      {
+        id: 6,
+        image: "../images/gujrati_kaadhi.jpg",
+        name: "Gujrati Kaadhi",
+        category: "lunch",
+        price: "100₹",
+        description:
+                   "Gujarati kadhi is a sweet tangy yogurt-based dish made with gram flour, spices, herbs and seasonings. This easy variant of kadhi is made without gram flour fritters (pakoda)."
+      },
+
+      {
+        id: 1,
+        image: "../images/pizza.jpg",
+        name: "pizza",
+        category: "evening",
+        price: "80₹",
+        description:
+           "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot ."    },
+      
+      {
+        id: 2,
+        image: "../images/burger.jpg",
+        name: "Burger",
+        category: "evening",
+        price: "80₹",
+        description:
+                   "A burger is a flat round mass of minced meat or vegetables, which is fried and often eaten in a bread roll."
+      },  
+      
+      {
+        id: 3,
+        image: "../images/French-Fries.jpg",
+        name: "French Fries",
+        category: "evening",
+        price: "80₹",
+        description:
+                   "French fries, also called chips, finger chips, fries, or French pommes frites, side dish or snack typically made from deep-fried potatoes that have been cut into various shapes, especially thin strips. Fries are often salted and served with other items, including ketchup, mayonnaise, or vinegar."
+      },      
+      {
+        id: 4,
+        image: "../images/poha-croquettes.jpg",
+        name: "poha croquettes",
+        category: "evening",
+        price: "60₹",
+        description:
+                   "Poha Croquettes are Indian Style deep fried balls made with flattened rice, cheese and potato. Perfect to serve as an appetizer ."
+      },      
+      {
+        id: 5,
+        image: "../images/sandwich.jpg",
+        name: "Sandwich",
+        category: "evening",
+        price: "60₹",
+        description:
+                   "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type."
+      },      
+      {
+        id: 6,
+        image: "../images/Momo.jpg",
+        name: "Momos",
+        category: "evening",
+        price: "60₹",
+        description:
+                   "Momo is a type of steamed dumpling with some form of filling, most commonly chicken (traditionally yak, but often chicken and goat) and it is originally from Tibet. Momo has become a delicacy in Tibet, Bhutan and the Indian communities in Darjeeling, Sikkim and Kalimpong."
+      },  
+      {
+        id: 1,
+        image: "../images/nonvegthali.jpg",
+        name: "Non-Veg Thali",
+        category: "dinner",
+        price: "180₹",
+        description:
+        "A non-vegetarian diet includes chicken, meat, eggs and fish. A non-vegetarian diet also has several health benefits because this type of food is rich in protein and vitamin B. Non-vegetarian food strengthens our muscles and helps them grow faster."    },
+        
+        {
+          id: 2,
+          image: "../images/Noodles.jpg",
+          name: "Noodles",
+          category: "dinner",
+          price: "80₹",
+          description:
+          "A noodle is a piece of pasta, especially a long, skinny one. You can eat noodles with butter and cheese or sauce, or slurp them from a bowl of soup. Noodles are cut or rolled from a dough that contains some kind of flour — wheat, buckwheat, and rice flour are all commonly used."
+        },
+        {
+          id: 3,
+          image: "../images/Veg Thali.jpg",
+          name: "Veg Thali",
+          category: "dinner",
+          price: "180₹",
+          description:
+          "A thali consists of: vegetables: a seasonal preparation. chutney: a condiment made with fruit, herbs, spices, and even vegetables and fish. raita: curds usually mixed with some kind of vegetable. pickles: most commonly made from raw mango, though that varies by region."
+        },
+        {
+          id: 4,
+          image: "../images/chicken_fajitas.jpg",
+          name: "chicken fajitas",
+          category: "dinner",
+          price: "180₹",
+          description:
+          "A fajita in Tex-Mex cuisine, is any stripped grilled meat, optionally served with stripped peppers and onions usually served on a flour or corn tortilla. The term originally referred to skirt steak, the cut of beef first used in the dish."
+        },
+        {
+          id: 5,
+          image: "../images/Garlic Shrimp.jpg",
+          name: "Garlic Shrimp",
+          category: "dinner",
+          price: "190₹",
+          description:
+          "For this recipe I typically use extra jumbo shrimp, which are 16-20 shrimp per pound. The shrimp get tossed in a pan with plenty of butter, minced garlic and salt and pepper. Be sure to use fresh minced garlic here, not the stuff that comes in a jar. It just doesn't taste the same!"
+        },
+        {
+          id: 1,
+          image: "../images/gulab-jamuns.jpg",
+          name: "Gulab jamuns",
+          category: "Desserts",
+          price: "80₹",
+          description: "Gulab jamun may satisfy your sugar cravings. But, it has nothing to offer in terms of nutrition and is nothing short of a junk food. Leading to weight gain, slow metabolism and blood sugar hikes. Since it is cooked in sugar, it is dangerous for diabetics and will adversely affect any weight loss plan."
+        },
+        {
+          id: 2,
+          image: "../images/Fudge Ice-Cream.jpg",
+          name: "Fudge Ice-Cream",
+          category: "Desserts",
+          price: "100₹",
+          description: "A cake is a sweet food made by baking a mixture of flour, eggs, sugar, and fat in an oven. Cakes may be large and cut into slices or small and intended for one person only.",
+        },    
+        {
+          id: 3,
+          image: "../images/Apple-Pie.jpg",
+          name: "Apple-Pie",
+          category: "Desserts",
+          price: "180₹",
+          description: "Apple pie: American icon, most popular pie, and the ONE dessert everyone has to make for Thanksgiving. With its flaky, buttery crust; tart-sweet apples, and wonderful cinnamon-y aroma, a slice of warm apple pie is the perfect end to any repast, from casual lunch to elegant dinner.",
+        },    
+        {
+          id: 4,
+          image: "../images/Custard.jpg",
+          name: "Custard",
+          category: "Desserts",
+          price: "120₹",
+          description: "Custard is a thick, rich, creamy sweet or savory dessert, made mixtures of eggs or egg yolks, milk or cream, flavorings (vanilla, nutmeg, etc.) and optionally, sweeteners (sugar, honey). Basic custards are thickened and set by eggs alone.",
+        },
+        
+        {
+          id: 1,
+          image: "../images/Mocktail.jpg",
+          name: "Mocktail",
+          category: " Beverages",
+          price: "140₹",
+          description:"They offer mocktails using fresh fruit juices. This delicious alcohol-free mocktail is a blend of lime, pineapple, and grenadine served over crushed ice. The teens get their own club with a mocktail bar and dance floor. A non-alcoholic mocktail is also available, but the price is the same.",
+        },
+        
+        {
+          id: 2,
+          image: "../images/Sparkling Drink.jpg",
+          name: "Sparkling Drink",
+          category: " Beverages",
+          price: "100₹",
+          description:"First and foremost, sparkling water does not contain calories or sugar. If you enjoy having a bit of “fizz” in your drink, carbonated water allows you to do so without the added calories. ",
+        },
+      ];
+        
+    
+  
+  export default Menu;
